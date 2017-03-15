6294376089ab1787d6e7435c27a550df2e363b31
rtxrulez <vafarulez@ya.ru>
2017-03-15 17:59:26 +0300

    fil

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import {Router, Route, hashHistory} from 'react-router'
+import App from './App'
+import About from './About'
+import './index.css'
+
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+    <div>
+    <Provider store={store}>
+        <Router history={hashHistory}>
+            <Route path="/" component={App}/>
+            <Route path="/about" component={About}/>
+        </Router>
+    </Provider>
+</div>, document.getElementById('root'));
